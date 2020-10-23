f8a9ec39f2e5de654771e921860c76d913408d3a
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-23 20:54:26 +0000

    finished with addition

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
+  def addition_results
+    # Parameters: {"first_num"=>"20", "second_num"=>"4"}
 
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first + @second
+
+    render({ :template => "math_templates/add_results.html.erb"})
+  end
+
+  def blank_add_form
+    render({ :template => "math_templates/add_form.html.erb"})
+  end
 end
