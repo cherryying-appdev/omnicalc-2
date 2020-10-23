cc199ba461f02d76a1f2c3de05fe9a40e586375d
yue-cherry-ying <cherryying@uchicago.edu>
2020-10-23 21:43:04 +0000

    finished with add, subtract, multiply and divide

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,40 @@ class ApplicationController < ActionController::Base
   def blank_add_form
     render({ :template => "math_templates/add_form.html.erb"})
   end
+
+  def blank_subtract_form
+    render({ :template => "math_templates/subtract_form.html.erb"})
+  end
+
+  def subtract_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @second - @first
+
+    render({ :template => "math_templates/subtract_results.html.erb"})
+  end
+
+  def blank_multiply_form
+    render({ :template => "math_templates/multiply_form.html.erb"})
+  end
+
+  def multiply_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first * @second
+
+    render({ :template => "math_templates/multiply_results.html.erb"})
+  end
+
+  def blank_divide_form
+    render({ :template => "math_templates/divide_form.html.erb"})
+  end
+
+  def divide_results
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @result = @first / @second
+
+    render({ :template => "math_templates/divide_results.html.erb"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   get("/wizard_add", { :controller =>  "application", :action =>  "addition_results" })
   get("/add", { :controller =>  "application", :action =>  "blank_add_form" })
+
+  get("/subtract", { :controller =>  "application", :action =>  "blank_subtract_form" })
+  get("/wizard_subtract", { :controller =>  "application", :action =>  "subtract_results" })
+
+  get("/multiply", { :controller =>  "application", :action =>  "blank_multiply_form" })
+  get("/wizard_multiply", { :controller =>  "application", :action =>  "multiply_results" })
+
+  get("/divide", { :controller =>  "application", :action =>  "blank_divide_form" })
+  get("/wizard_divide", { :controller =>  "application", :action =>  "divide_results" })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
